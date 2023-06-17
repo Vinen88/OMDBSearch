a38367415392dee94b462b1925f5af0ba13541f0
Jason Robock <jrobock@ualberta.ca>
2023-06-17 14:52:13 -0600

    first commit

---
+++ moveSearch.js
@@ -0,0 +1,43 @@
+import { addListener, createSlice } from '@reduxjs/toolkit';
+
+import axios from 'axios';
+
+const omdbUrl = 'http://www.omdbapi.com/';
+
+export const movieSlice = createSlice({
+    name: 'movie',
+    initialState: {
+        results: [],
+    },
+    reducers: {
+        setResults: (state, action) => {
+            state.results = action.payload;
+        },
+    },
+});
+
+export const searchMovies = query => dispatch => {
+    axios.get( omdbUrl, {
+        params: {
+            'apikey': process.env.REACT_APP_OMDB_API_KEY,
+            's': query,
+            'type': 'movie',
+        },
+    },
+    ).then( response => {
+        if (response.data['Response'] === 'True') {
+            dispatch(setResults(response.data['Search']));
+        } else {
+            alert(response.data['Error']);
+        }
+        }).catch( err => {
+            alert(err);
+        })
+
+    };
+
+export const { setResults } = movieSlice.actions;
+
+export const selectMovies = state => state.movie.results;
+
+export default movieSlice.reducer;
---
+++ resultsList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import Result from "./results";
+import { selectMovies } from "../../app/movieSearch";
+
+function ResultsList(props){
+
+    return(
+        <>
+
+        </>
+    );
+}
+
+export default ResultsList;
