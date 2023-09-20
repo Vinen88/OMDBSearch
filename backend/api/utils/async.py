4cb7ae5019dd9b1edf794a98da6cfae86bbc7c06
Jason Robock <jrobock@ualberta.ca>
2023-09-20 14:42:45 -0600

    minor changes to async tinkering

--- backend/api/utils/async.py
+++ backend/api/utils/async.py
@@ -6,6 +6,16 @@ import os
 load_dotenv()
 
 OMDB_API_KEY = os.getenv("OMDB_API_KEY")
+DOG_DIE_KEY = os.getenv("DOG_DIE_KEY")
+TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+TMDB_TOKEN = os.getenv("TMDB_TOKEN")
+OMDB_URL_BASE = "http://www.omdbapi.com/?i={}&apikey={}"
+DDD_URL_SEARCH = "https://www.doesthedogdie.com/dddsearch?q={}"
+DDD_URL = "https://www.doesthedogdie.com/media/{}"
+
+
+def create_urls(imdbID, title, year):
+    pass
 
 
 async def getMovie(url, session):
@@ -18,7 +28,8 @@ async def getMovie(url, session):
 
 
 async def getMovieDetails(imdbID, title, year):
-    omdb_url = f"http://www.omdbapi.com/?i={imdbID}&apikey={OMDB_API_KEY}"
+    omdb_url = OMDB_URL_BASE.format(imdbID, OMDB_API_KEY)
+
     async with aiohttp.ClientSession() as session:
         response = await getMovie(omdb_url, session)
         if response is not None:
